cbd677555ab03c5c7f4b2b8543e2c08fe983ccde
Rick <141774554+Sttorm1@users.noreply.github.com>
2023-08-18 04:43:22 -0300

    Add files via upload
    calculadora de media feita em Python, sendo meu primeiro código após alguns dias estudando programação

---
+++ CalcDeMedia.py
@@ -0,0 +1,16 @@
+def calculadora_de_media():
+    # Solicitar ao usuário para inserir as notas
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    nota3 = float(input("Digite a terceira nota: "))
+    
+    # Calcular a média das notas
+    media = (nota1 + nota2 + nota3) / 3
+    
+    # Exibir o resultado ao usuário
+    print(f"A média das notas é: {media:.2f}")
+
+# Chamar a função da calculadora
+calculadora_de_media()
+
+#é o começo dos meus estudos de programação, então to começando pelo basico :3   
